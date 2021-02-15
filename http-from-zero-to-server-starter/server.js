01ec029056b3fce14b4e852444935b19e4b3b3eb
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-02-15 17:52:39 -0600

    Final changes

--- http-from-zero-to-server-starter/server.js
+++ http-from-zero-to-server-starter/server.js
@@ -1,21 +1,43 @@
-const http = require('http');
+const http = require("http");
 console.log("hi");
-const server = http.createServer(function(req, res) {
-    if (req.url === '/OK') {
-        console.log("Inbound 'OK' request being processed...");
-        res.writeHead(200);
-        // res.end();
-     } else {
-        // console.log("404 not found");
-        res.writeHead(404);
-        res.write("oops");
-        // res.end();
-    }
-    res.end();
-})
-
+const server = http.createServer(function (req, res) {
+  if (req.url === "/OK") {
+    console.log("Inbound 'OK' request being processed...");
+    res.writeHead(200);
+    // res.end();
+  } else if (req.url === "/Bad-Request") {
+    res.writeHead(400);
+    res.write("...bad request");
+  } else if (req.url === "/Created") {
+    res.writeHead(201);
+    res.write("created");
+  } else if (req.url === "/Forbidden") {
+    res.writeHead(403);
+    res.write("forbidden");
+  } else if (req.url === "/Found") {
+    res.writeHead(302);
+    res.write("found");
+  } else if (req.url === "/Gateway-Timeout") {
+    res.writeHead(504);
+    res.write("gateway timeout");
+  } else if (req.url === "/Internal-Server-Error") {
+    res.writeHead(500);
+    res.write("internal server error");
+  } else if (req.url === "/Moved-Permanently") {
+    res.writeHead(301);
+    res.write("moved perm.");
+  } else if (req.url === "/Unauthorized") {
+    res.writeHead(401);
+    res.write("unauthorized");
+  } else {
+    // console.log("404 not found");
+    res.writeHead(404);
+    res.write("oops");
+    // res.end();
+  }
+  res.end();
+});
 const port = 3000;
-
 server.listen(port, () => {
-    console.log(`Listening for requests on port ${port}`);
-})
+  console.log(`Listening for requests on port ${port}`);
+});
